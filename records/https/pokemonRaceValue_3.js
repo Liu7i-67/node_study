a91275ed2c3f11f460c59247081e20e3cf9b32a7
liuqi <liuqi@neurongenius.com>
2022-10-09 12:39:47 +0800

    feat:change bug

--- records/https/pokemonRaceValue_3.js
+++ records/https/pokemonRaceValue_3.js
@@ -2,7 +2,7 @@
  * @Author: liu7i
  * @Date: 2022-08-08 17:13:34
  * @Last Modified by: liu7i
- * @Last Modified time: 2022-08-10 09:59:19
+ * @Last Modified time: 2022-10-09 12:35:19
  *
  * 获取神百指定url宝可梦基本信息
  */
@@ -11,7 +11,7 @@ import http from "node:https";
 import cheerio from "cheerio";
 
 // 即将抓取的连接
-const url = "https://wiki.52poke.com/wiki/%E6%95%B2%E9%9F%B3%E7%8C%B4";
+const url = "https://www.pokemon.cn/play/pokedex/001";
 
 function getText(dom, select) {
   let text = dom(select).prop("innerText");
@@ -40,26 +40,41 @@ function getData(str) {
   const $ = cheerio.load(str);
   const obj = {};
   // 名称
-  obj.name = getText($, "h1#firstHeading");
+  obj.name = getText($, "p.pokemon-slider__main-name");
   // 全国图鉴编号
-  obj.no = getText(
-    $,
-    "div.mw-parser-output table:nth-child(2) table.roundy.fulltable th.textblack.bgwhite"
-  );
+  obj.no = getText($, "p.pokemon-slider__main-no");
   // 属性
   obj.attribute = getText(
     $,
-    "div.mw-parser-output table:nth-child(2) tbody tr:nth-child(3) td:nth-child(1) table table a>span>span:last-child"
+    "div.pokemon-type div.pokemon-type__type:first-child span"
   );
-  // 种族值
-  obj.raceValue = {
-    hp: getText($, "tr.bgl-HP th div:nth-child(2)"),
-    atk: getText($, "tr.bgl-攻击 th div:nth-child(2)"),
-    def: getText($, "tr.bgl-防御 th div:nth-child(2)"),
-    sAtk: getText($, "tr.bgl-特攻 th div:nth-child(2)"),
-    sDef: getText($, "tr.bgl-特防 th div:nth-child(2)"),
-    speed: getText($, "tr.bgl-速度 th div:nth-child(2)"),
-  };
+  // 属性2
+  obj.attribute2 = getText(
+    $,
+    "div.pokemon-type div.pokemon-type__type:nth-child(2) span"
+  );
+  // 身高
+  obj.height = getText($, "div.pokemon-info__height span.pokemon-info__value");
+  // 分类
+  obj.category = getText(
+    $,
+    "div.pokemon-info__category span.pokemon-info__value"
+  );
+  // 体重
+  obj.weight = getText($, "div.pokemon-info__weight span.pokemon-info__value");
+  // 图片
+  obj.img = `https://www.pokemon.cn${$("div.pokemon-img img:last-child").prop(
+    "src"
+  )}`;
+  // // 种族值
+  // obj.raceValue = {
+  //   hp: getText($, "tr.bgl-HP th div:nth-child(2)"),
+  //   atk: getText($, "tr.bgl-攻击 th div:nth-child(2)"),
+  //   def: getText($, "tr.bgl-防御 th div:nth-child(2)"),
+  //   sAtk: getText($, "tr.bgl-特攻 th div:nth-child(2)"),
+  //   sDef: getText($, "tr.bgl-特防 th div:nth-child(2)"),
+  //   speed: getText($, "tr.bgl-速度 th div:nth-child(2)"),
+  // };
   console.log("-------------------宝可梦信息-----------------");
   console.log(obj);
 }
